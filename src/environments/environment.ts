3380e5737cb609cce51540bef38472995b0fee70
Oscar Fernández <racsofernandez@gmail.com>
2025-10-31 07:44:44 +0100

    Clear the environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,16 @@
 
 export const environment = {
     production: false,
-    url: 'https://api.themoviedb.org/3',
-    apiKey: process.env['NG_APP_THEMOVIEDB_API_KEY'],
-    imgPath: 'https://image.tmdb.org/t/p',
+    url: '',
+    apiKey: '',
+    imgPath: '',
     firebaseConfig: {
-        apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
-        authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
-        projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'],
-        storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
-        messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'],
-        appId: process.env['NG_APP_FIREBASE_APP_ID']
+        apiKey: '',
+        authDomain: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: '',
+        appId: ''
     }
 };
 
