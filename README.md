cf6d98ba14eb60aab8025029311f5becf912a54c
Oscar Fernández <racsofernandez@gmail.com>
2025-11-10 14:43:02 +0100

    Change name of application to SeriesApp

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RacsoPeliculasApp
+# RacsoSeriesApp
 Aplicación de ionic del curso de udemy "Legacy: ionic 6: Crear aplicaciones IOS, Android con Angular"
 
 # Android Studio
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'PeliculasApp',
+  appName: 'SeriesApp',
   webDir: 'www',
   server: {
     androidScheme: 'https'
