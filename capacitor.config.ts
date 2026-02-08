9d76fa1883fd7ca8c972fd8c908a0532b70c1f72
Oscar Fernández <racsofernandez@gmail.com>
2026-02-08 09:04:26 +0100

    Configure background color for status bar and navigation bar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -12,13 +12,6 @@ const config: CapacitorConfig = {
       skipNativeAuth: false,
       providers: ['google.com'],
     },
-    // Añadir configuración para el Status Bar y Navigation Bar
-    StatusBar: {
-      backgroundColor: '#141414', // Color de fondo oscuro
-    },
-    NavigationBar: {
-      backgroundColor: '#141414', // Color de fondo oscuro
-    },
   },
 };
 
